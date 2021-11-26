303cecb9027c8a0ff922e9ccfeba2e9753f5c3ac
surendarsvg <66377855+surendarsvg@users.noreply.github.com>
2021-11-26 16:34:38 +0530

    Add files via upload

---
+++ sampleTest1/samplecode.java
@@ -0,0 +1,10 @@
+package sampleTest1;
+
+public class samplecode {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+                System.out.println("Welcomr, its working ");
+	}
+
+}
